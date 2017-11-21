8728d3accdaa82c9d6f51d6c9a08cf5766cb5db3
Andrew Moore [el16ajm] <el16ajm@eentpc93.leeds.ac.uk>
2017-11-21 15:25:41 +0000

    Initial Commit

---
+++ JetGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JetGame
+//
+//  Created by Andrew Moore [el16ajm] on 21/11/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JetGame/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  JetGame
+//
+//  Created by Andrew Moore [el16ajm] on 21/11/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
